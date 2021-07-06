7b6a9bbce5c94a337e8c810faa3d986b2fe795d9
Natalia Rossini <naty.rossini@hotmail.com>
2021-07-06 17:13:26 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import Expenses from './components/Expenses'
+
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14)
+    },
+    {
+      id: 'e2',
+      title: 'Car Insurance',
+      amount: 294.12,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: 'e3',
+      title: 'Now TV',
+      amount: 100,
+      date: new Date(2019, 3, 2)
+    }
+  ]
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+  <Expenses items={expenses} />
+  )
 }
 
 export default App;
