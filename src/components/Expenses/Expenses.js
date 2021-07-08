7d054af51c9776612fc9e4f124eba4655c3b130c
Natalia Rossini <naty.rossini@hotmail.com>
2021-07-08 20:55:19 +0100

    add bars

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,11 @@
-import ExpensesList from './ExpensesList';
+import React, { useState } from 'react';
+
 import Card from '../UI/Cards';
-import './Expenses.css';
 import ExpensesFilter from "./ExpensesFilter";
-import { useState } from "react";
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
+
 const Expenses = (props) => {
 
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -10,26 +13,26 @@ const Expenses = (props) => {
   //setting a year when user selects on dropdown
   const addFilterByYear = (selectedYear) => {
     setFilteredYear(selectedYear);
-    console.log(props.items)
   };
 
 
   //displaying the expenses of each year
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
   
       return (
-        <li>
+        <div>
           <Card className='expenses'>
             <ExpensesFilter 
               selected={filteredYear} 
               onChangeFilter={addFilterByYear} 
             />
+          <ExpensesChart expenses={filteredExpenses} />
           <ExpensesList items={filteredExpenses}/>
           </Card>
-        </li>
+        </div>
       );
-}
+};
 export default Expenses;
